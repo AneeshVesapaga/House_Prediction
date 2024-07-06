1b55a6811a063dbb09a7c021dd040fcf3c51b7bd
AneeshVesapaga <164468484+AneeshVesapaga@users.noreply.github.com>
2024-07-06 15:26:15 +0530

    Update project.py

--- project.py
+++ project.py
@@ -24,9 +24,9 @@ st.title("HOUSE PRICE PREDICTION")
 #image_path = os.path.join(os.path.dirname(__file__), "ino_img.jpg")
 
 # Input fields
-SquareFeet = st.number_input("Enter Area of house in square feet", min_value=600, max_value=5000, step=50)
-Bedrooms = st.number_input("Enter the number of bedrooms", min_value=0, max_value=5, step=1)
-Bathrooms = st.number_input("Enter the number of bathrooms", min_value=0, max_value=5, step=1)
+SquareFeet = st.number_input("Enter Area of house in square feet", min_value=2400, max_value=6000, step=50)
+Bedrooms = st.number_input("Enter the number of bedrooms", min_value=0, max_value=6, step=1)
+Bathrooms = st.number_input("Enter the number of bathrooms", min_value=0, max_value=6, step=1)
 Neighborhood = st.radio("Select type of neighbourhood", ['Rural', 'Urban', 'Suburb'])
 neighbor = 1 if Neighborhood == "Rural" else 2 if Neighborhood == "Urban" else 3
 YearBuilt = st.number_input("Enter year of Construction of property", min_value=2020, max_value=2070, step=1)
