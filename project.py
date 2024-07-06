cf7edeb0b333fe68fcec516a25fca33cb7336018
AneeshVesapaga <164468484+AneeshVesapaga@users.noreply.github.com>
2024-07-06 14:53:30 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import numpy as np
 import pickle
@@ -19,7 +20,7 @@ except Exception as e:
 # Title and image
 st.title("HOUSE PRICE PREDICTION")
 
-# Specify the path to your image file
+# Specify the path to your image file using __file__ method
 image_path = os.path.join(os.path.dirname(__file__), "ino_img.jpeg")
 
 # Display the image using st.image()
@@ -34,5 +35,17 @@ st.write("Here is the image 'ino_img.jpeg'")
 
 # Input fields
 SquareFeet = st.number_input("Enter Area of house in square feet", min_value=600, max_value=5000, step=50)
-Bedrooms = st.number
+Bedrooms = st.number_input("Enter the number of bedrooms", min_value=0, max_value=5, step=1)
+Bathrooms = st.number_input("Enter the number of bathrooms", min_value=0, max_value=5, step=1)
+Neighborhood = st.radio("Select type of neighbourhood", ['Rural', 'Urban', 'Suburb'])
+neighbor = 1 if Neighborhood == "Rural" else 2 if Neighborhood == "Urban" else 3
+YearBuilt = st.number_input("Enter year of Construction of property", min_value=1900, max_value=2030, step=1)
 
+# Prediction
+if st.button("PREDICT PRICE"):
+    try:
+        price = model.predict([[SquareFeet, Bedrooms, Bathrooms, neighbor, YearBuilt]])
+        st.write("The price for the flat with given details is Rs.", price[0])
+    except Exception as e:
+        st.error("An error occurred during prediction:")
+        st.error(e)
